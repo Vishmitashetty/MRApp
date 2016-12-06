3b23a8ebd9f6e17f7fc880156a3b521c1579c7b2
Vishmita Shetty <vishmitashetty94@gmail.com>
2016-12-06 15:39:09 +0530

    MRApp
    Using HTML,Bootstrap,AngularJS with Web API

---
+++ MovieReviews/myapp.js
@@ -0,0 +1,55 @@
+﻿(function () {
+
+    angular.module("movie",[])
+   .controller("movieapp", function ($scope, $http) {
+       
+       
+       $scope.getData = function () {
+
+           var i;
+           var n = 0;
+           var dat = new Array();
+          var m = document.getElementById("type").value; //type value(movie,series)
+           var array = $scope.searchparams.split(','); // converting string to array
+
+           //looping through each string
+           for (i = 0; i < array.length; i++) {
+               var searchword = array[i];
+               var len = array[i].length;
+               if (len != null) {
+
+                   $http.get('https://www.omdbapi.com/?s=' + array[i] + '&type=' + m).success(function (data) {
+
+                       document.getElementById("result").style.display = 'block';
+                       $scope.datas = data;
+                       console.log(data);
+                       var count = 0;
+
+                       //iterating through response data array of object
+                       angular.forEach(data.Search, function (obj) {
+
+                           $http.get('https://www.omdbapi.com/?i=' + obj.imdbID + '&plot=short&r=json&tomatoes=true').success(function (response) {
+
+                               //storing data in array for each object
+
+                               dat[count] = response;
+                               console.log(response);
+                               count++;
+                           })
+                           $scope.child = dat;
+                       });
+
+
+                   }).error(function (data) {
+                       $scope.error = "An error has occured while adding! " + data;
+                   });
+
+                  
+
+               }
+             
+           }
+       }
+   })
+
+})();
